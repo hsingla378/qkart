8edbdab1eddf7848e7a2d6827942a7ae84befedc
Himanshu Singla <37644800+hsingla378@users.noreply.github.com>
2024-04-11 18:13:51 +0530

    Update backend link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `https://himanshu-qkart-frontend-594e1847ef86.herokuapp.com/api/v1`,
+  endpoint: `https://qkart-2n5b.onrender.com/api/v1`,
 };
 
 function App() {
